b630e50a57cf6aa5f1c44783ede52dadb308337c
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2025-02-07 21:00:13 +0700

    refactor: state, type, refactor
    - add filter state to jotai
    - add user type
    - refactor filter component

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -3,22 +3,9 @@
 import { Button } from "./ui/button";
 import { Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger } from "./ui/drawer";
 import { useAtom } from "jotai";
-import { filterTypeAtom, filterTagsAtom } from "@/state/filterState";
-
-const tags = ["halu", "gws", "klarifikasi", "???", "explicit"];
+import { filterTypeAtom, filterTagsAtom, tagsAtom, typesAtom } from "@/state/filterState";
 
 export default function Filter() {
-  const [filterType, setFilterType] = useAtom(filterTypeAtom);
-  const [filterTags, setFilterTags] = useAtom(filterTagsAtom);
-
-  const handleFilterType = (type: string) => {
-    setFilterType((prevType: string[]) => (prevType.includes(type) ? prevType.filter((t) => t !== type) : [...prevType, type]));
-  };
-
-  const handleFilterTags = (tags: string) => {
-    setFilterTags((prevType: string[]) => (prevType.includes(tags) ? prevType.filter((t) => t !== tags) : [...prevType, tags]));
-  };
-
   return (
     <div>
       <div className="lg:hidden">
@@ -31,45 +18,7 @@ export default function Filter() {
               <DrawerHeader>
                 <DrawerTitle>filter</DrawerTitle>
               </DrawerHeader>
-              <div className="flex flex-col gap-4">
-                <div>
-                  <p>tipe?</p>
-                  <div className="flex gap-2">
-                    <Button
-                      className={filterType.includes("static") ? "translate-x-boxShadowX translate-y-boxShadowY" : "shadow-shadow hover:shadow-none"}
-                      variant={"active"}
-                      size={"sm"}
-                      onClick={() => handleFilterType("static")}
-                    >
-                      static
-                    </Button>
-                    <Button
-                      className={filterType.includes("dynamic") ? "translate-x-boxShadowX translate-y-boxShadowY" : "shadow-shadow hover:shadow-none"}
-                      variant={"active"}
-                      size={"sm"}
-                      onClick={(e) => handleFilterType("dynamic")}
-                    >
-                      dynamic
-                    </Button>
-                  </div>
-                </div>
-                <div>
-                  <p>tags</p>
-                  <div className="flex gap-2 flex-wrap">
-                    {tags.map((tag) => (
-                      <Button
-                        className={filterTags.includes(tag) ? "translate-x-boxShadowX translate-y-boxShadowY opacity-75" : "shadow-shadow hover:shadow-none"}
-                        variant={"active"}
-                        key={tag}
-                        size={"sm"}
-                        onClick={() => handleFilterTags(tag)}
-                      >
-                        {tag}
-                      </Button>
-                    ))}
-                  </div>
-                </div>
-              </div>
+              <FilterList />
             </div>
           </DrawerContent>
         </Drawer>
@@ -79,45 +28,59 @@ export default function Filter() {
           <div>
             <p className="font-bold">filter</p>
           </div>
-          <div className="flex flex-col gap-4">
-            <div>
-              <p>tipe?</p>
-              <div className="flex gap-2">
-                <Button
-                  className={filterType.includes("static") ? "translate-x-boxShadowX translate-y-boxShadowY max-lg:opacity-75" : "shadow-shadow hover:shadow-none"}
-                  variant={"active"}
-                  size={"sm"}
-                  onClick={() => handleFilterType("static")}
-                >
-                  static
-                </Button>
-                <Button
-                  className={filterType.includes("dynamic") ? "translate-x-boxShadowX translate-y-boxShadowY max-lg:opacity-75" : "shadow-shadow hover:shadow-none"}
-                  variant={"active"}
-                  size={"sm"}
-                  onClick={(e) => handleFilterType("dynamic")}
-                >
-                  dynamic
-                </Button>
-              </div>
-            </div>
-            <div>
-              <p>tags</p>
-              <div className="flex gap-2 flex-wrap">
-                {tags.map((tag) => (
-                  <Button
-                    className={filterTags.includes(tag) ? "translate-x-boxShadowX translate-y-boxShadowY max-lg:opacity-75" : "shadow-shadow hover:shadow-none"}
-                    variant={"active"}
-                    key={tag}
-                    size={"sm"}
-                    onClick={() => handleFilterTags(tag)}
-                  >
-                    {tag}
-                  </Button>
-                ))}
-              </div>
-            </div>
-          </div>
+          <FilterList />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function FilterList() {
+  const [types] = useAtom(typesAtom);
+  const [filterType, setFilterType] = useAtom(filterTypeAtom);
+  const [tags] = useAtom(tagsAtom);
+  const [filterTags, setFilterTags] = useAtom(filterTagsAtom);
+
+  const handleFilterType = (type: string) => {
+    setFilterType((prevType: string[]) => (prevType.includes(type) ? prevType.filter((t) => t !== type) : [...prevType, type]));
+  };
+
+  const handleFilterTags = (tags: string) => {
+    setFilterTags((prevTag: string[]) => (prevTag.includes(tags) ? prevTag.filter((t) => t !== tags) : [...prevTag, tags]));
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <p>tipe?</p>
+        <div className="flex gap-2">
+          {types.map((type) => (
+            <Button
+              key={type}
+              className={filterType.includes(type) ? "translate-x-boxShadowX translate-y-boxShadowY" : "shadow-shadow"}
+              variant={"active"}
+              size={"sm"}
+              onClick={() => handleFilterType(type)}
+            >
+              {type}
+            </Button>
+          ))}
+        </div>
+      </div>
+      <div>
+        <p>tags</p>
+        <div className="flex gap-2 flex-wrap">
+          {tags.map((tag) => (
+            <Button
+              className={filterTags.includes(tag) ? "translate-x-boxShadowX translate-y-boxShadowY" : "shadow-shadow"}
+              variant={"active"}
+              key={tag}
+              size={"sm"}
+              onClick={() => handleFilterTags(tag)}
+            >
+              {tag}
+            </Button>
+          ))}
         </div>
       </div>
     </div>
--- src/state/filterState.ts
+++ src/state/filterState.ts
@@ -3,3 +3,7 @@ import { atom } from "jotai";
 export const filterTypeAtom = atom<string[]>([]);
 
 export const filterTagsAtom = atom<string[]>([]);
+
+export const tagsAtom = atom<string[]>(["halu", "gws", "klarifikasi", "???", "explicit"]);
+
+export const typesAtom = atom<string[]>(["static", "dynamic"]);
